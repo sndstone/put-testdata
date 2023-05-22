d4c167f48e0f39f05fa0e683301d79a9a6e0cc65
Bryan Steenwinkel <89138099+sndstone@users.noreply.github.com>
2023-05-22 14:31:45 +0200

    Update put-testdata.py

--- put-testdata.py
+++ put-testdata.py
@@ -3,15 +3,34 @@ import concurrent.futures
 import os
 import uuid
 from prettytable import PrettyTable
+import json
 
-#Asks inputs to run run the script
-BUCKET_NAME = input("Enter the bucket name: ")
-S3_ENDPOINT_URL = input("Enter the S3 endpoint URL, (EXAMPLE http://example.com:443): ")
-AWS_ACCESS_KEY_ID = input("Enter the AWS access key ID: ")
-AWS_SECRET_ACCESS_KEY = input("Enter the AWS secret access key: ")
+# Function to read credentials from JSON file
+def read_credentials_from_json(file_path):
+    with open(file_path, "r") as json_file:
+        credentials = json.load(json_file)
+        return credentials
+
+# Asks inputs to run run the script
+JSON_IMPORT = input("Do you want to import JSON file for configuration? (yes/no): ")
+
+if JSON_IMPORT.lower() == "yes":
+    JSON_FILE_PATH = input("Enter the JSON file path: ")
+    credentials = read_credentials_from_json(JSON_FILE_PATH)
+    BUCKET_NAME = credentials["bucket_name"]
+    S3_ENDPOINT_URL = credentials["s3_endpoint_url"]
+    AWS_ACCESS_KEY_ID = credentials["aws_access_key_id"]
+    AWS_SECRET_ACCESS_KEY = credentials["aws_secret_access_key"]
+else:
+    BUCKET_NAME = input("Enter the bucket name: ")
+    S3_ENDPOINT_URL = input("Enter the S3 endpoint URL, (EXAMPLE http://example.com:443): ")
+    AWS_ACCESS_KEY_ID = input("Enter the AWS access key ID: ")
+    AWS_SECRET_ACCESS_KEY = input("Enter the AWS secret access key: ")
+
 OBJECT_SIZE = int(input("Enter the size of the objects in bytes: "))
 VERSIONS = int(input("Enter the number of versions to be created: "))
 OBJECTS_COUNT = int(input("Enter the number of objects to be placed: "))
+OBJECT_PREFIX = input("Enter the prefix for the objects: ")
 
 # Create an S3 client
 s3 = boto3.client("s3",
@@ -22,8 +41,8 @@ s3 = boto3.client("s3",
 
 # Define a function that creates a single object in the bucket
 def create_object(arg):
-    # Generate a random object key
-    object_key = str(uuid.uuid4())
+    # Generate a random object key with the prefix
+    object_key = f"{OBJECT_PREFIX}{str(uuid.uuid4())}"
 
     # Generate random content for the object
     object_content = os.urandom(OBJECT_SIZE)
